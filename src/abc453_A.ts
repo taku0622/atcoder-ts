9f5e30bd5ca1d063a6e130fa7a9ba75c73e8147e
taku <hayatsutkm@gmail.com>
2026-05-06 13:18:30 +0900

    remove node_modules

---
+++ src/abc453_A.ts
@@ -0,0 +1,8 @@
+import * as fs from "fs";
+const input = fs.readFileSync(0, "utf8").trim();
+const [n, s] = input.split(/\s+/)
+const arr = s.split("");
+while(true){
+    let i = 0;
+    if(arr[i] === '')
+}
