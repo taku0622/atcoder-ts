9e82b98a37a0024a097d954305e5803502d8e002
taku <hayatsutkm@gmail.com>
2026-05-06 13:41:39 +0900

    done 453_A

--- src/abc453_A.ts
+++ src/abc453_A.ts
@@ -2,7 +2,14 @@ import * as fs from "fs";
 const input = fs.readFileSync(0, "utf8").trim();
 const [n, s] = input.split(/\s+/)
 const arr = s.split("");
-while(true){
-    let i = 0;
-    if(arr[i] === '')
+let ans = "";
+let f: boolean = true;
+for(const v of arr){
+    if(f && v === 'o'){
+        continue;
+    } else if(v !== 'o'){
+        f = false;
+    }
+    ans += v;
 }
+console.log(ans);
