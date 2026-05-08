baf235f52597c5c9b8e705a97a0ea571c0b26a62
taku <hayatsutkm@gmail.com>
2026-05-08 23:51:28 +0900

    done ABC455A

---
+++ src/abc455_A.ts
@@ -0,0 +1,10 @@
+import * as fs from "fs";
+
+const input = fs.readFileSync(0, "utf8").trim();
+const [a, b, c] = input.split(/\s+/).map(Number);;
+
+let answer = "No";
+if(a !== b && b === c){
+    answer = "Yes";
+}
+console.log(answer);
