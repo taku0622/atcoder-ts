0919eb853699e4ee0c874f416d0c3312350e0c72
taku <hayatsutkm@gmail.com>
2026-05-08 23:55:37 +0900

    revise

--- src/abc455_A.ts
+++ src/abc455_A.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 
 const input = fs.readFileSync(0, "utf8").trim();
-const [a, b, c] = input.split(/\s+/).map(Number);;
+const [a, b, c] = input.split(/\s+/).map(Number);
 
 let answer = "No";
 if(a !== b && b === c){
