58071f1b927dabdde7001c19a2ffdac1d4a792a6
taku <hayatsutkm@gmail.com>
2026-06-14 12:53:02 +0900

    clean 462_A

--- src/abc462_A.ts
+++ src/abc462_A.ts
@@ -2,12 +2,10 @@ import * as fs from "fs";
 
 const input = fs.readFileSync(0, "utf8").trim().split(/\s+/);
 
-const words = input[0].split("");
 let ans = "";
 
-for(let w of words){
-    const regex = /[0-9]/;
-    if(regex.test(w)){
+for(const w of input[0]){
+    if("0" <= w && w <= "9"){
         ans += w;
     }
 }
