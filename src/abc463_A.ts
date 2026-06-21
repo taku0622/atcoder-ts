87db060b9ea668151b6555d15273cf2b1ebfe769
taku <hayatsutkm@gmail.com>
2026-06-21 21:51:09 +0900

    clean 463_A

--- src/abc463_A.ts
+++ src/abc463_A.ts
@@ -3,7 +3,7 @@ import * as fs from "fs";
 const input = fs.readFileSync(0, "utf8").trim();
 const [x, y] = input.split(/\s+/).map(Number);
 
-if(x / (x + y) === 0.64){
+if(x * 9 === y * 16){
     console.log("Yes");
 } else {
     console.log("No");
